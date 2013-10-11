dfe31bc6ab01eca11cbfc2105b799968777f4300
Olwaro <hoarau.olivier@gmail.com>
2013-10-11 20:54:18 +0200

    Switch to rangebased for-loop... ... and use unique_ptr to ensure RAII idiom

--- fun_with_sfml2/main.cpp
+++ fun_with_sfml2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <memory>
 
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
@@ -9,15 +10,17 @@ using namespace std;
 
 int main()
 {
+	// Window initialization
 	sf::ContextSettings cs;
 	cs.antialiasingLevel = 8;
 
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Fun with SFML2", 7U, cs);
 	window.setFramerateLimit(60);
 
+	// Some utilities
 	sf::Clock clk;
-	list<sf::CircleShape*> circles;
-	vector<sf::Color> colours;
+	list<unique_ptr<sf::CircleShape>> circles;
+	vector<sf::Color> colours; // List initializer not supported yet by VS2012 :'(
 		colours.push_back(sf::Color::Blue); 
 		colours.push_back(sf::Color::Green);
 		colours.push_back(sf::Color::Magenta);
@@ -26,16 +29,19 @@ int main()
 	int ci = 0;
 	float maxRadius = 400;
 
+	// Main loop
     while (window.isOpen())
     {
+		// Event processing
         sf::Event event;
         while (window.pollEvent(event))
         {
 			if (event.type == sf::Event::MouseMoved)
 			{
-				if (clk.getElapsedTime().asMilliseconds() > 45)
+				if (clk.getElapsedTime().asMilliseconds() > 45) // Limit circle creation frequency
 				{
-					sf::CircleShape *tmp = new sf::CircleShape(10);
+					// Create a circle with a random colour under mouse cursor
+					unique_ptr<sf::CircleShape> tmp(new sf::CircleShape(10));
 					tmp->setFillColor(sf::Color::Transparent);
 					tmp->setOutlineThickness(1);
 					tmp->setOutlineColor(colours[(ci++)%colours.size()]);
@@ -43,32 +49,37 @@ int main()
 					tmp->setOrigin(tmp->getRadius(), tmp->getRadius());
 					tmp->setPosition((float)event.mouseMove.x, (float)event.mouseMove.y);
 
-					circles.push_back(tmp);
+					// Use move sementic "to give circle's ownership" to the container
+					circles.push_back(move(tmp));
 
-					//cout << "x:" << event.mouseMove.x << " y:" << event.mouseMove.y << endl;
 					clk.restart();
 				}
 			}
+
 			if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-		circles.remove_if([maxRadius](sf::CircleShape* c) { return c->getRadius() > maxRadius; });
+		// Get rid of circles over a given radius
+		circles.remove_if([maxRadius](unique_ptr<sf::CircleShape>& c) { return c->getRadius() > maxRadius; });
 		
-
         window.clear();
-		for(auto i = circles.begin(); i != circles.end(); ++i)
+
+		// Update radius et draw each circle
+		for(auto &circle : circles)
 		{
-			(*i)->setRadius((*i)->getRadius()+2);
-			(*i)->setOrigin((*i)->getRadius(), (*i)->getRadius());
+			circle->setRadius(circle->getRadius()+2);
+			circle->setOrigin(circle->getRadius(), circle->getRadius());
 			
-			sf::Color c = sf::Color((*i)->getOutlineColor());
-			c.a = 255 * (1 - (*i)->getRadius()/(maxRadius+2));
+			sf::Color c = sf::Color(circle->getOutlineColor());
+			c.a = (sf::Uint8) (255 * (1 - circle->getRadius()/(maxRadius+2)));
 
-			(*i)->setOutlineColor(c);
+			circle->setOutlineColor(c);
 
-			window.draw(**i);
+			window.draw(*circle);
 		}
+
+		// Display the whole picture on screen
         window.display();
     }
 
